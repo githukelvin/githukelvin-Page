fa9f30ffd3635b1712dadf1bb71ed4cfc3081842
githukelvin <kelvingithu019@gmail.com>
2024-01-25 17:34:51 +0300

    resolved hmburger issue

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,21 @@ import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
-  <div>
     <TheHeader />
     <div class="router">
       <router-view></router-view>
     </div>
     <TheFooter />
-  </div>
 </template>
 
 <style scoped lang="scss">
-div {
-  overflow-x: hidden;
-  height: fit-content;
-  @media (max-width: 575.98px){
-    height: 100%;
-  }
-}
+
 .router {
   margin-top: 5em;
+  @media (max-width:575.98px){
+    margin-top:6em;
+  }
+    
+  
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,28 +8,60 @@
             <h1>Githukelvin</h1>
           </router-link>
         </div>
-        <div class="nav__menu">
-          <ul>
-            <li>
-              <router-link class="link" to="/">Home</router-link>
-            </li>
-            <li>
-              <router-link class="link" to="/about">About</router-link>
-            </li>
-            <li>
-              <router-link class="link" to="/project">Projects</router-link>
-            </li>
-            <li>
-              <router-link class="link" to="/contact">Contact</router-link>
-            </li>
-          </ul>
+
+        <div  class="nav__menu">
+         
+        <ul >
+                <li>
+                  <router-link class="link" to="/">Home</router-link>
+                </li>
+                <li>
+                  <router-link class="link" to="/about">About</router-link>
+                </li>
+                <li>
+                  <router-link class="link" to="/project">Projects</router-link>
+                </li>
+                <li>
+                  <router-link class="link" to="/contact">Contact</router-link>
+                </li>
+              </ul>
         </div>
-        <div class="ham"></div>
+        <div @click="toggleClass"  :class="{ 'active': istoggled }"  class="ham"></div>
       </div>
     </div>
   </div>
+   <div class="mmenu" :class="{ 'active': istoggled }">
+              <ul >
+              <li>
+                <router-link class="link" to="/">Home</router-link>
+              </li>
+              <li>
+                <router-link class="link" to="/about">About</router-link>
+              </li>
+              <li>
+                <router-link class="link" to="/project">Projects</router-link>
+              </li>
+              <li>
+                <router-link class="link" to="/contact">Contact</router-link>
+              </li>
+            </ul>
+          </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { watch,ref } from 'vue';
+let istoggled = ref(false)
+
+function toggleClass(){
+  istoggled.value = !istoggled.value
+}
+// onclick update istoggled
+watch(istoggled, (newValue) => {
+  if(newValue){
+    istoggled.value= newValue
+  }
+})
+
+</script>
 
 <style lang="scss" scoped></style>
