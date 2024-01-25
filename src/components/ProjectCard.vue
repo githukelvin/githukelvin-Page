efaffa40238a97d0ef99946ab4dafe7690a71796
githukelvin <kelvingithu019@gmail.com>
2024-01-25 13:30:27 +0300

    modified count

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="card">
+  <div class="card" >
     <img :src="project.image" alt="here" class="card__image" />
     <div class="details">
-      <h2>{{project.title }}</h2>
-      <p> {{ project.description }} </p>
+      <h2>{{project }}</h2>
+      <p> {{ project.Desc }} </p>
       <h3>Technologies</h3>
       <ul class="techs" v-for="tech in project.techs" :key="tech.index">
         <li>{{ tech }}</li>
 
       </ul>
       <div class="btns">
-        <router-link target="_blank" :to="project.live">Live Site
+        <a  :href="project.live" target="_blank">Live Site
           <img src="../assets/images/new_tab.gif" alt="new tab"/>
-        </router-link>
-          <router-link target="_blank" :to="project.github">Github Repo
+        </a>
+          <a :href="project.github" target="_blank" >Github Repo
             <img src="../assets/images/github.gif" alt="">
-          </router-link>
+          </a>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { defineProps } from 'vue';
+// import { defineProps } from 'vue';
 let project =defineProps({
+  title: String,
+  image: String,
+  Desc: String,
+  live: String,
   github:String,
-  live:String,
-  image:String,
-  title:String,
-  description:String,
   techs:Array
 
 })
--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -1,7 +1,9 @@
 <script setup>
 import ProjectCard from '@/components/ProjectCard.vue'
 import data from '@/data.json';
-console.log(data[0].live)
+
+const filterProjects = (type) => data.filter((project) => project.category === type);
+console.log(filterProjects('ML'))
 </script>
 
 <template>
@@ -15,10 +17,10 @@ console.log(data[0].live)
   </div>
   <div class="container">
     <div class="bar">
-      <button class="all">All Projects({{ data.length }})</button>
-      <button class="frontend">Frontend Mentor(2)</button>
-      <button class="personalP">Personal(2)</button>
-      <button class="ml">Machine Learning(2)</button>
+      <button @click="filterProjects('all')" class="all">All Projects({{ data.length }})</button>
+      <button @click="filterProjects('mentor')" class="frontend">Frontend Mentor({{ filterProjects('mentor').length }})</button>
+      <button @click="filterProjects('personal')" class="personalP">Personal({{  filterProjects('personal').length }})</button>
+      <button @click="filterProjects('ML')" class="ml">Machine Learning({{filterProjects('ML').length}})</button>
     </div>
     <div class="projects" >
       <ProjectCard v-for="project in data" :key="project.category" :project="project" />
